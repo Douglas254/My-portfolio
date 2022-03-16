0e4988b93f4eb8cbfe5276d3b0e6f1198e624251
tinegaCollins <collinstinega003@gmail.com>
2022-03-16 21:26:02 +0300

    new commit

--- script.js
+++ script.js
@@ -70,106 +70,13 @@ const projectDivs = [...document.querySelectorAll(".realProjects")]
 
 for (let i = 0; i < projectDivs.length; i++) {
     projectDivs[i].addEventListener("click",()=>{
-        console.log(projectDivs.splice(projectDivs.indexOf(projectDivs[i]) + 1,projectDivs.length))
+       let currentClick =  projectDivs.indexOf(projectDivs[i]);
+       console.log(currentClick)
+       for (let j = currentClick + 1; j < projectDivs.length; j++) {
+           console.log(projectDivs[j])
+           // i swear to god i dont know how this shit worked 🤣
+           projectDivs[j].classList.toogle("leftMove")
+       }
     })
 }
-
-// const project1 = document.querySelector(".project1")
-// const project2 = document.querySelector(".project2")
-// const projectt = document.querySelector("#projects2")
-// const project3 = document.querySelector(".project3")
-// const project4 = document.querySelector(".project4")
-// const project5 = document.querySelector(".project5")
-// const project6 = document.querySelector(".project6")
-// const project7 = document.querySelector(".project7")
-// projectt.addEventListener("mouseover",function(e){
-//     if(e.target.className == ("realProjects project1"||"complete" || "projectTitle" || "languages" || "sourceCode" || "contributions")){
-//         project2.classList.add("project2Hover")
-//         project3.classList.add("project3Hover")
-//         project4.classList.add("project4Hover")
-//         project5.classList.add("project5Hover")
-//         project6.classList.add("project6Hover")
-//         project7.classList.add("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseout",function(e){
-//     if(e.target.className == "realProjects project1" || "complete" || "projectTitle" || "languages" || "sourceCode" || "contributions"){
-//         project2.classList.remove("project2Hover")
-//         project3.classList.remove("project3Hover")
-//         project4.classList.remove("project4Hover")
-//         project5.classList.remove("project5Hover")
-//         project6.classList.remove("project6Hover")
-//         project7.classList.remove("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseover",function(e){
-//     if(e.target.className == ("realProjects project2"||"complete" || "projectTitle" || "languages" || "sourceCode" || "contributions")){
-//         project3.classList.add("project3Hover")
-//         project4.classList.add("project4Hover")
-//         project5.classList.add("project5Hover")
-//         project6.classList.add("project6Hover")
-//         project7.classList.add("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseout",function(e){
-//     if(e.target.className == "realProjects project2" || "complete" || "projectTitle" || "languages" || "sourceCode" || "contributions"){
-//         project3.classList.remove("project3Hover")
-//         project4.classList.remove("project4Hover")
-//         project5.classList.remove("project5Hover")
-//         project6.classList.remove("project6Hover")
-//         project7.classList.remove("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseover",function(e){
-//     if(e.target.className == ("realProjects project3"||"complete" || "projectTitle" || "languages" || "sourceCode" || "contributions")){
-//         project4.classList.add("project4Hover")
-//         project5.classList.add("project5Hover")
-//         project6.classList.add("project6Hover")
-//         project7.classList.add("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseout",function(e){
-//     if(e.target.className == "realProjects project3" || "complete" || "projectTitle" || "languages" || "sourceCode" || "contributions"){
-//         project4.classList.remove("project4Hover")
-//         project5.classList.remove("project5Hover")
-//         project6.classList.remove("project6Hover")
-//         project7.classList.remove("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseover",function(e){
-//     if(e.target.className == ("realProjects project4"||"complete" || "projectTitle" || "languages" || "sourceCode" || "contributions")){
-//         project5.classList.add("project5Hover")
-//         project6.classList.add("project6Hover")
-//         project7.classList.add("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseout",function(e){
-//     if(e.target.className == "realProjects project4" || "complete" || "projectTitle" || "languages" || "sourceCode" || "contributions"){
-//         project5.classList.remove("project5Hover")
-//         project6.classList.remove("project6Hover")
-//         project7.classList.remove("project7Hover")
-//     }
-// });
-// projectt.addEventListener("mouseover",function(e){
-//     if(e.target.className == ("realProjects project5"||"complete" || "projectTitle" || "languages" || "sourceCode" || "contributions")){
-//         project6.classList.add("project6Hover")
-//         project7.classList.add("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseout",function(e){
-//     if(e.target.className == "realProjects project5" || "complete" || "projectTitle" || "languages" || "sourceCode" || "contributions"){
-//         project6.classList.remove("project6Hover")
-//         project7.classList.remove("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseover",function(e){
-//     if(e.target.className == ("realProjects project6"||"complete" || "projectTitle" || "languages" || "sourceCode" || "contributions")){
-//         project7.classList.add("project7Hover")
-//     }
-// })
-// projectt.addEventListener("mouseout",function(e){
-//     if(e.target.className == "realProjects project6" || "complete" || "projectTitle" || "languages" || "sourceCode" || "contributions"){
-//         project7.classList.remove("project7Hover")
-//     }
-// })
 
